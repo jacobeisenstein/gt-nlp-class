4a918293a122fed4510e8b3f32ae9be0500cf61b
jacobeisenstein <jacobe@gmail.com>
2013-08-12 19:00:03 -0400

    m

---
+++ projects/proj-1/scorer.py
@@ -0,0 +1,63 @@
+import sys
+from collections import defaultdict
+
+#scoring script for text classification. 
+#first argument should be a key file, containing space-separated filename and label
+#second argument should be a response file, containing just the predicted label
+
+def main():
+    key = sys.argv[1]
+    response = sys.argv[2]
+    counts = getConfusion(key,response)
+    printScoreMessage(counts)
+
+def getConfusion(keyfilename,responsefilename):
+    counts = defaultdict(int)
+    with open(keyfilename,'r') as keyfile:
+        with open(responsefilename,'r') as resfile:
+            for keyline in keyfile:
+                keyline = keyline.split(' ')[-1].rstrip()
+                resline = resfile.readline().rstrip()
+                counts[tuple((keyline,resline))] += 1
+    return(counts)
+
+def accuracy(counts):
+    true_pos = 0.0
+    total = 0.0
+    keyclasses = set([x[0] for x in counts.keys()])
+    resclasses = set([x[1] for x in counts.keys()])
+    for keyclass in keyclasses:
+        for resclass in resclasses:
+            c = counts[tuple((keyclass,resclass))]
+            total += float(c)
+            if resclass==keyclass:
+                true_pos += float(c)
+    return(true_pos/total)
+
+def printScoreMessage(counts):
+    true_pos = 0
+    total = 0
+
+    keyclasses = set([x[0] for x in counts.keys()])
+    resclasses = set([x[1] for x in counts.keys()])
+    print "%d classes in key: %s" % (len(keyclasses),keyclasses)
+    print "%d classes in response: %s" % (len(resclasses),resclasses)
+    print "confusion matrix"
+    print "key\t"+"\t".join(resclasses)
+    for i,keyclass in enumerate(keyclasses):
+        print keyclass+"\t",
+        for j,resclass in enumerate(resclasses):
+            c = counts[tuple((keyclass,resclass))]
+            #countarr[i,j] = c
+            print "{}\t".format(c),
+            total += float(c)
+            if resclass==keyclass:
+                true_pos += float(c)
+        print ""
+    print "----------------"
+    print "accuracy: %.4f = %d/%d\n" % (true_pos / total, true_pos,total)
+
+
+if __name__ == "__main__":
+    main()
+
